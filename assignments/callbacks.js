48f576c66caba5b7a35f6b02af97c1084a81b47c
Roenz Aberin <42616880+raberin@users.noreply.github.com>
2019-10-16 13:46:50 -0700

    Revert "Caleb allard"

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'yo-yo', 'Pencil'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
@@ -41,34 +41,24 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'yo-yo', 'Pencil'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length);
 }
-getLength(items,test);
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-    return cb(arr.length - 1);
 }
-last(items,test);
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  const sum = x + y;  
-  return cb(sum);
 }
-sumNums(1,2,test);
+
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.\
-  return cb(x*y);
+  // multiplyNums multiplies two numbers and passes the result to the callback.
 }
-multiplyNums(2,4,test);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb(list.includes(item));
 }
-contains('Gum', items, test);//true
-contains('tea', items, test);//false
-
 
 /* STRETCH PROBLEM */
 
@@ -76,23 +66,4 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  let myArray = [];
-  
-  array.forEach(e=>{
-    if(!myArray.includes(e)){
-      myArray.push(e);
-    }
-  })
-
-
-  cb(myArray);
 }
-removeDuplicates( items,test);
-
-// My test callBack
-function test(p){
-  console.log(p);
-}
-
-//
-
--- assignments/closure.js
+++ assignments/closure.js
@@ -12,53 +12,21 @@
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  let count = 0;
-  const counter = () =>{
-    count ++;
-    return count;
-  }
   // 2- Declare a function `counter`. It should increment and return `count`.
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
-  return counter();
 };
-// const myCounter = counterMaker();
-
-// console.log(myCounter);//1
-// console.log(myCounter);//2
-
+// Example usage: const myCounter = counterMaker();
+// myCounter(); // 1
+// myCounter(); // 2
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
-const counterMakerTwoPointZero = (limt) => {
-  let count = 0;
-  const counter = () => {
-    if (count < limit){
-      return count++;
-    }
-  }
-
-}
-
-
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-
-  let counter = 0;
-  const counterObj = {
-    increment:function() {
-      counter++;
-      return counter;
-    },
-    decrement: function(){
-      counter--;
-      return counter--;
-    }
-  }
-
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
